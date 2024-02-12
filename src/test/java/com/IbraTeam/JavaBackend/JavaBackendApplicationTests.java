66dafd28db71a8d5d2674d2aa64372e97cebf98c
3eight70 <gbhfns90@gmail.com>
2024-02-12 23:39:02 +0700

    Initial commit

---
+++ src/test/java/com/IbraTeam/JavaBackend/JavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IbraTeam.JavaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
