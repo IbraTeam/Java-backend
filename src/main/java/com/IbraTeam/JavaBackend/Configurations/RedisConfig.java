3c958e68c455eb48227e9ba4834ccb42b4adea6f
Vasilev Danil <111416100+3eight70@users.noreply.github.com>
2024-02-16 00:24:58 +0700

    Update RedisConfig.java

--- src/main/java/com/IbraTeam/JavaBackend/Configurations/RedisConfig.java
+++ src/main/java/com/IbraTeam/JavaBackend/Configurations/RedisConfig.java
@@ -3,6 +3,7 @@ package com.IbraTeam.JavaBackend.Configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
 
 @Component
