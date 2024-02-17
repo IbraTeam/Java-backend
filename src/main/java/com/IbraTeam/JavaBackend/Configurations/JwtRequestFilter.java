a5ee331ffb82c71178aa84034039a99779ebb65a
Vasilev Danil <111416100+3eight70@users.noreply.github.com>
2024-02-17 15:59:37 +0700

    Update JwtRequestFilter.java

--- src/main/java/com/IbraTeam/JavaBackend/Configurations/JwtRequestFilter.java
+++ src/main/java/com/IbraTeam/JavaBackend/Configurations/JwtRequestFilter.java
@@ -35,7 +35,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         boolean tokenInRedis = false;
 
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-Auth-Token");
 
         try {
