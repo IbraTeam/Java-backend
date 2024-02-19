e6cd7ed350565fef472bf6824086d969cf950606
Aleksey Shumkov <shumleshas@gmail.com>
2024-02-19 17:37:25 +0700

    Исправлена валидация аудитории

--- src/main/java/com/IbraTeam/JavaBackend/Models/dto/KeyDTO.java
+++ src/main/java/com/IbraTeam/JavaBackend/Models/dto/KeyDTO.java
@@ -1,5 +1,6 @@
 package com.IbraTeam.JavaBackend.Models.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.util.UUID;
@@ -7,6 +8,8 @@ import java.util.UUID;
 @Data
 public class KeyDTO {
     private UUID Id;
+
+    @Pattern(regexp = "^[1-9][0-9]*((\\([0-9]+\\))[0-9]*)*$\n", message = "Номер аудитории может содержать только цифры и скобки")
     private String room;
 
 
